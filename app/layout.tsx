6333e617ef675decdd5d98951018e845ad8881fb
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-10-04 23:56:59 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Dev Geeks | Invites',
-  description: 'A community to develop',
+  title: 'Dev Geeks | Invitations',
+  description: 'Dev geeks platform is a vibrant hub for developers of all backgrounds and expertise levels to come together and create, learn, and grow. Whether you are a seasoned coder or just starting your programming journey, you will find a welcoming and supportive community eager to help you achieve your goals',
 }
 
 export default function RootLayout({
