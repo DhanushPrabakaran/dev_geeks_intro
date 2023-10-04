c32d7b732d07004e48a5fbfac6d5c7041528a075
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-10-04 20:59:49 +0530

    git

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,238 @@
-import Image from 'next/image'
-import Hero from '@/Components/Hero'
-import Header from '@/Components/Header'
-import Footer from '@/Components/Footer'
-import Groups from '@/Components/Groups'
-Header
+import Image from "next/image";
+
+import Innovation from "@/public/Innovation.gif"
+
+import UIux from "@/public/UIUXdesign.gif"
+
 export default function Home() {
   return (
     <div>
-      <Header/>
-       <Hero/>
-       <Groups/>
-       
-       <Footer/>
+      <div className="navbar bg-neutral text-neutral-content justify-center">
+        <h1 className="text-3xl font-bold">
+          <span className="text-secondary pr-1 italic">Dev </span> Geeks
+        </h1>
+      </div>
+      {/*  <Hero /> */}
+      <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row justify-center align-middle">
+        <div
+        className="items-center flex flex-col flex-wrap">
+          <h1 className="text-5xl font-bold italic text-primary text-center">A community Platform</h1>
+          <p className="py-6 text-center ">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-secondary w-fit font-extrabold italic">Get Started</button>
+        </div>
+        
+        <Image src={UIux} alt={""} />
+      </div>
     </div>
-    
-  )
+      {/* <Groups /> */}
+      <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row justify-center align-middle">
+        <div
+        className="items-center flex flex-col flex-wrap lg:w-2/4 ">
+            <h1 className="text-5xl font-bold italic text-primary text-center">Invite Links</h1>
+      <div
+      className='w-fit flex flex-wrap justify-center'>
+            <a href="https://quizizz.com/join?ref=header_tab">
+              <div
+                className=" relative max-sm:w-44 h-12 w-56 bg-cover  rounded-md text-center flex justify-center items-center m-2 cursor-pointer animate-[fade-in_2s_ease-in-out]"
+                style={{
+                  backgroundImage:
+                    "url(https://i.pinimg.com/originals/b6/a1/64/b6a164fe3c74eeb8fae8de7ad4b1d3ef.gif)",
+                }}
+              >
+                <div className=" rounded-md absolute bg-neutral w-full h-full bg-opacity-80"></div>
+                <h1 className=" z-10 inline-block text-center  hover:text-xl duration-300 font-bold hover:text-secondary">
+                  Buiness quiz
+                </h1>
+              </div>
+            </a>
+      
+            <a href="https://quizizz.com/join?ref=header_tab">
+              <div
+                className=" relative max-sm:w-44 h-12 w-56 bg-cover  rounded-md text-center flex justify-center items-center m-2 cursor-pointer animate-[fade-in_2s_ease-in-out]"
+                style={{
+                  backgroundImage:
+                    "url(https://i.pinimg.com/originals/b6/a1/64/b6a164fe3c74eeb8fae8de7ad4b1d3ef.gif)",
+                }}
+              >
+                <div className=" rounded-md absolute bg-neutral w-full h-full bg-opacity-80"></div>
+                <h1 className=" z-10 inline-block text-center  hover:text-xl duration-300 font-bold hover:text-secondary">
+                  Buiness quiz
+                </h1>
+              </div>
+            </a>
+      
+            <a href="https://quizizz.com/join?ref=header_tab">
+              <div
+                className=" relative max-sm:w-44 h-12 w-56 bg-cover  rounded-md text-center flex justify-center items-center m-2 cursor-pointer animate-[fade-in_2s_ease-in-out]"
+                style={{
+                  backgroundImage:
+                    "url(https://i.pinimg.com/originals/b6/a1/64/b6a164fe3c74eeb8fae8de7ad4b1d3ef.gif)",
+                }}
+              >
+                <div className=" rounded-md absolute bg-neutral w-full h-full bg-opacity-80"></div>
+                <h1 className=" z-10 inline-block text-center  hover:text-xl duration-300 font-bold hover:text-secondary">
+                  Buiness quiz
+                </h1>
+              </div>
+            </a>
+      
+            <a href="https://quizizz.com/join?ref=header_tab">
+              <div
+                className=" relative max-sm:w-44 h-12 w-56 bg-cover  rounded-md text-center flex justify-center items-center m-2 cursor-pointer animate-[fade-in_2s_ease-in-out]"
+                style={{
+                  backgroundImage:
+                    "url(https://i.pinimg.com/originals/b6/a1/64/b6a164fe3c74eeb8fae8de7ad4b1d3ef.gif)",
+                }}
+              >
+                <div className=" rounded-md absolute bg-neutral w-full h-full bg-opacity-80"></div>
+                <h1 className=" z-10 inline-block text-center  hover:text-xl duration-300 font-bold hover:text-secondary">
+                  Buiness quiz
+                </h1>
+              </div>
+            </a>
+      
+            <a href="https://quizizz.com/join?ref=header_tab">
+              <div
+                className=" relative max-sm:w-44 h-12 w-56 bg-cover  rounded-md text-center flex justify-center items-center m-2 cursor-pointer animate-[fade-in_2s_ease-in-out]"
+                style={{
+                  backgroundImage:
+                    "url(https://i.pinimg.com/originals/b6/a1/64/b6a164fe3c74eeb8fae8de7ad4b1d3ef.gif)",
+                }}
+              >
+                <div className=" rounded-md absolute bg-neutral w-full h-full bg-opacity-80"></div>
+                <h1 className=" z-10 inline-block text-center  hover:text-xl duration-300 font-bold hover:text-secondary">
+                  Buiness quiz
+                </h1>
+              </div>
+            </a>
+      
+            <a href="https://quizizz.com/join?ref=header_tab">
+              <div
+                className=" relative max-sm:w-44 h-12 w-56 bg-cover  rounded-md text-center flex justify-center items-center m-2 cursor-pointer animate-[fade-in_2s_ease-in-out]"
+                style={{
+                  backgroundImage:
+                    "url(https://i.pinimg.com/originals/b6/a1/64/b6a164fe3c74eeb8fae8de7ad4b1d3ef.gif)",
+                }}
+              >
+                <div className=" rounded-md absolute bg-neutral w-full h-full bg-opacity-80"></div>
+                <h1 className=" z-10 inline-block text-center  hover:text-xl duration-300 font-bold hover:text-secondary">
+                  Buiness quiz
+                </h1>
+              </div>
+            </a>
+      
+            <a href="https://quizizz.com/join?ref=header_tab">
+              <div
+                className=" relative max-sm:w-44 h-12 w-56 bg-cover  rounded-md text-center flex justify-center items-center m-2 cursor-pointer animate-[fade-in_2s_ease-in-out]"
+                style={{
+                  backgroundImage:
+                    "url(https://i.pinimg.com/originals/b6/a1/64/b6a164fe3c74eeb8fae8de7ad4b1d3ef.gif)",
+                }}
+              >
+                <div className=" rounded-md absolute bg-neutral w-full h-full bg-opacity-80"></div>
+                <h1 className=" z-10 inline-block text-center  hover:text-xl duration-300 font-bold hover:text-secondary">
+                  Buiness quiz
+                </h1>
+              </div>
+            </a>
+      
+            <a href="https://quizizz.com/join?ref=header_tab">
+              <div
+                className=" relative max-sm:w-44 h-12 w-56 bg-cover  rounded-md text-center flex justify-center items-center m-2 cursor-pointer animate-[fade-in_2s_ease-in-out]"
+                style={{
+                  backgroundImage:
+                    "url(https://i.pinimg.com/originals/b6/a1/64/b6a164fe3c74eeb8fae8de7ad4b1d3ef.gif)",
+                }}
+              >
+                <div className=" rounded-md absolute bg-neutral w-full h-full bg-opacity-80"></div>
+                <h1 className=" z-10 inline-block text-center  hover:text-xl duration-300 font-bold hover:text-secondary">
+                  Buiness quiz
+                </h1>
+              </div>
+            </a>
+      
+            <a href="https://quizizz.com/join?ref=header_tab">
+              <div
+                className=" relative max-sm:w-44 h-12 w-56 bg-cover  rounded-md text-center flex justify-center items-center m-2 cursor-pointer animate-[fade-in_2s_ease-in-out]"
+                style={{
+                  backgroundImage:
+                    "url(https://i.pinimg.com/originals/b6/a1/64/b6a164fe3c74eeb8fae8de7ad4b1d3ef.gif)",
+                }}
+              >
+                <div className=" rounded-md absolute bg-neutral w-full h-full bg-opacity-80"></div>
+                <h1 className=" z-10 inline-block text-center  hover:text-xl duration-300 font-bold hover:text-secondary">
+                  Buiness quiz
+                </h1>
+              </div>
+            </a>
+      
+            <a href="https://quizizz.com/join?ref=header_tab">
+              <div
+                className=" relative max-sm:w-44 h-12 w-56 bg-cover  rounded-md text-center flex justify-center items-center m-2 cursor-pointer animate-[fade-in_2s_ease-in-out]"
+                style={{
+                  backgroundImage:
+                    "url(https://i.pinimg.com/originals/b6/a1/64/b6a164fe3c74eeb8fae8de7ad4b1d3ef.gif)",
+                }}
+              >
+                <div className=" rounded-md absolute bg-neutral w-full h-full bg-opacity-80"></div>
+                <h1 className=" z-10 inline-block text-center  hover:text-xl duration-300 font-bold hover:text-secondary">
+                  Buiness quiz
+                </h1>
+              </div>
+            </a>
+      
+            <a href="https://quizizz.com/join?ref=header_tab">
+              <div
+                className=" relative max-sm:w-44 h-12 w-56 bg-cover  rounded-md text-center flex justify-center items-center m-2 cursor-pointer animate-[fade-in_2s_ease-in-out]"
+                style={{
+                  backgroundImage:
+                    "url(https://i.pinimg.com/originals/b6/a1/64/b6a164fe3c74eeb8fae8de7ad4b1d3ef.gif)",
+                }}
+              >
+                <div className=" rounded-md absolute bg-neutral w-full h-full bg-opacity-80"></div>
+                <h1 className=" z-10 inline-block text-center  hover:text-xl duration-300 font-bold hover:text-secondary">
+                  Buiness quiz
+                </h1>
+              </div>
+            </a>
+      
+            <a href="https://quizizz.com/join?ref=header_tab">
+              <div
+                className=" relative max-sm:w-44 h-12 w-56 bg-cover  rounded-md text-center flex justify-center items-center m-2 cursor-pointer animate-[fade-in_2s_ease-in-out]"
+                style={{
+                  backgroundImage:
+                    "url(https://i.pinimg.com/originals/b6/a1/64/b6a164fe3c74eeb8fae8de7ad4b1d3ef.gif)",
+                }}
+              >
+                <div className=" rounded-md absolute bg-neutral w-full h-full bg-opacity-80"></div>
+                <h1 className=" z-10 inline-block text-center  hover:text-xl duration-300 font-bold hover:text-secondary">
+                  Buiness quiz
+                </h1>
+              </div>
+            </a>
+            </div>
+    </div>
+    <Image src={Innovation} alt={""} />
+  </div>
+
+</div>
+      {/* <Footer /> */}
+      <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+      <aside className="items-center grid-flow-col">
+      <h1 className=" font-bold"><span className="text-secondary italic">Dev</span> Geeks</h1>
+        <p>&copy;Copyright 2023-<a 
+         className="btn-link text-sm "
+          href="https://github.com/DhanushPrabakaran">@Dhanushprabakaran</a></p>
+         
+      </aside>
+      
+    </footer>
+    </div>
+  );
 }
