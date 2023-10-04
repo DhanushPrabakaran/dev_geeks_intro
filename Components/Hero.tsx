71cbd4475a98a0611d0b353a4ebd5e85c100e416
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-10-04 20:24:21 +0530

    content

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import Image from "next/image";
+import UIux from "@/public/UI-UX design.gif"
 const Hero = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content text-center">
-    <div className="max-w-md">
-      <h1 className="text-5xl font-bold">Hello there</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+      <div className="hero-content flex-col lg:flex-row justify-center align-middle">
+        <div
+        className="items-center flex flex-col flex-wrap">
+          <h1 className="text-5xl font-bold italic text-primary text-center">A community Platform</h1>
+          <p className="py-6 text-center ">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-secondary w-fit font-extrabold italic">Get Started</button>
+        </div>
+        
+        <Image src={UIux} alt={""} />
+      </div>
     </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image'
 import Hero from '@/Components/Hero'
 import Header from '@/Components/Header'
+import Footer from '@/Components/Footer'
+import Groups from '@/Components/Groups'
 Header
 export default function Home() {
   return (
     <div>
       <Header/>
        <Hero/>
+       
+       <Groups/>
+       
+       <Footer/>
     </div>
     
   )
