207596c43ef86e00f6e9b27d079e2a44bf7a80a9
DhanushPrabakaran <dhanushprabakaran18@gmail.com>
2023-10-04 15:38:55 +0530

    initial

---
+++ Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="navbar bg-neutral text-neutral-content justify-center">
+      <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    </div>
+  );
+};
+
+export default Header;
